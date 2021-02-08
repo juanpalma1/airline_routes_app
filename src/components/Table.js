c6620f653415dec5c6da8d98298a79da0bf5554d
Juan Palma <juanpedropalma@hotmail.com>
2021-02-08 01:17:37 +0000

    Pagination added to Table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,27 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-const Table = ({ columns, rows: routes, format }) => {
+const Table = ({
+  columns,
+  rows: routes,
+  format,
+  className,
+  perPage = 25
+}) => {
+  const [ page, setPage ] = useState(0);
+  
+  const headerCells = columns.map(column => {
+    return <th key={column.name}>{column.name}</th>;
+  });
+  
+  const start = page * perPage;
+  
+  const bodyRows = routes.slice(start, start + perPage).map(route => {
+    const row = columns.map(({ property }) => {
+      const value = route[property];
+      return <td key={property + value}>{format(property, value)}</td>;
+    });
+    
+    return <tr key={Object.values(route).join(':')}>{row}</tr>;
+  });
+  
+  const paginationMessage = `Showing ${start + 1} - ` +
+    `${start + perPage} of ${routes.length} total routes`;
+    
+  const previousPage = () => {
+    setPage(page - 1);
+  };
+  
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+  
+  const disablePreviousButton = () => page === 0;
+  
+  const disableNextButton = () => {
+    return routes.length - start <= perPage;
+  };
+    
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Airline</th>
-          <th>Source</th>
-          <th>Destination</th>
-        </tr>
-      </thead>
-      <tbody>
-        {routes.map(({ airline, src, dest }) => {
-          return (
-            <tr>
-              <td>{format(columns[0]['property'], airline)}</td>
-              <td>{format(columns[1]['property'], src)}</td>
-              <td>{format(columns[2]['property'], dest)}</td>
-            </tr>
-          );
-        })}
-      </tbody>  
-    </table>
+    <div>
+      <table className={className}>
+        <thead>
+          <tr>{headerCells}</tr>
+        </thead>
+        <tbody>{bodyRows}</tbody>
+      </table>
+      <div className="pagination">
+        <p>{paginationMessage}</p>
+        <button type="button"
+                onClick={previousPage}
+                disabled={disablePreviousButton()}
+        >
+          Previous Page
+        </button>
+        <button type="button"
+                onClick={nextPage}
+                disabled={disableNextButton()}
+        >
+          Next Page
+        </button>
+      </div>
+    </div>
   );
 };
 
